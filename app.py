694fa488381d4b7907b24d7e52f0d15e5cfbee44
fracalove <gabrielfracalossi1@hotmail.com>
2024-03-27 19:41:07 -0300

    commit 2

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 import mysql.connector
 
 #Twilio - SMS/Whatsapp
-sid = ""
-token = ""
+sid = "sua sid"
+token = "seu token"
 client = Client(sid, token)
 
 #Flask
@@ -26,16 +26,21 @@ connectBD = mysql.connector.connect(host='localhost', database='usuarios', user=
 #info email
 msg = email.message.Message()
 s = smtplib.SMTP('smtp.gmail.com: 587')
-password = 'xlhqeykiahfjwzxn'
+password = 'seu codigo do smtp'
 msg['From'] = 'notifyblood@gmail.com'
 
 #notificacao pelo tipo sanguineo
-def notificacao(tipo_sanguineo, usuarios, local):
+def notificacao(tipo_sanguineo, local, sangue):
+    if connectBD.is_connected():
+        cursor = connectBD.cursor()
+        cursor.execute('select * from usuario;')
+        usuarios = cursor.fetchall()
+    
     if tipo_sanguineo < "35":
             cont = 0
             for user in usuarios:
                 cont += 1
-                if str(user[5]) == "B-":
+                if str(user[5]) == sangue:
 
                     #envio de Whatsapp
                     client.messages.create(from_='whatsapp:+14155238886', body=f"NotifyBlood: {str(user[1])}, estamos precisando da sua doação! Por favor, quando for possível se direcionar ao endereço: {local}", to="whatsapp:+55"+str(user[3]))
@@ -55,7 +60,8 @@ def notificacao(tipo_sanguineo, usuarios, local):
                     msg['To'] = str(user[2])
                     msg.add_header('Content-Type', 'text/html')
                     msg.set_payload(corpo_email)
-
+                    
+                    s = smtplib.SMTP('smtp.gmail.com: 587')
                     s.starttls()
                     s.login(msg['From'], password)
                     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
@@ -103,7 +109,6 @@ def contatos():
             
         if connectBD.is_connected():
             cursor.close()
-            connectBD.close()
             
         flash('Mensagem enviada com sucesso!')
         return redirect('/')
@@ -114,14 +119,14 @@ def cadastro():
     global logado
     if request.method == 'POST':
         email_cadastro = request.form.get('email')
-        nome = request.form.get('name')
+        name = request.form.get('name')
         idade = request.form.get('idade')
         sangue = request.form.get('sangue')
         telefone = request.form.get('telefone')
         sexo = request.form.get('sexo')
 
 #login de administrador
-        if email_cadastro == 'admin@admin' and nome == 'admin' and telefone == '99999999999':
+        if email_cadastro == 'admin@admin' and name == 'admin' and telefone == '99999999999':
             logado = True
             return render_template('admin.html')
 
@@ -144,11 +149,11 @@ def cadastro():
 #cadastro do usuario no banco de dados
         if connectBD.is_connected():
             cursor = connectBD.cursor()
-            cursor.execute(f"insert into usuario values (default, '{nome}', '{email_cadastro}', '{telefone}', '{idade}', '{sangue}', '{sexo}');")
+            cursor.execute(f"insert into usuario values (default, '{name}', '{email_cadastro}', '{telefone}', '{idade}', '{sangue}', '{sexo}');")
 
 #envio das informações de contato para email
             corpo_email = f"""
-            <p>Olá, {nome}!</p>
+            <p>Olá, {name}!</p>
             <p>Estamos muito felizes que você resolveu participar do nosso projeto!</p>
             <p>Fique atento às notificações que enviamos, através dela você saberá quando estivermos precisando de doação.</p>
             <br>
@@ -157,7 +162,7 @@ def cadastro():
             <p>Email: notifyblood@gmail.com</p>
             """
 
-            msg['Subject'] = f"Bem-vindo ao NotifyBlood, {nome}!"
+            msg['Subject'] = f"Bem-vindo ao NotifyBlood, {name}!"
             msg['To'] = email_cadastro
             msg.add_header('Content-Type', 'text/html')
             msg.set_payload(corpo_email)
@@ -168,9 +173,8 @@ def cadastro():
 
         if connectBD.is_connected():
             cursor.close()
-            connectBD.close()
             
-        flash(f"Cadastro realizado com sucesso! Seja bem-vindo(a), {nome}")    
+        flash(f"Cadastro realizado com sucesso! Seja bem-vindo(a), {name}")    
         return redirect('/')
 
 #----- ADMIN -----
@@ -186,36 +190,35 @@ def admin():
     On = request.form.get('O-')
     local = request.form.get('local')
 
-#recolhe informações do banco de dados
     if connectBD.is_connected():
         cursor = connectBD.cursor()
         cursor.execute('select * from usuario;')
         usuarios = cursor.fetchall()
 
 #envio de notificação
-        #A+
-        notificacao(Ap, usuarios, local)
+    #A+    
+    notificacao(Ap, local, "A+")
 
-        #A-
-        notificacao(An, usuarios, local)
+    #A-    
+    notificacao(An, local, "A-")
 
-        #B+
-        notificacao(Bp, usuarios, local)
-    
-        #B-
-        notificacao(Bn, usuarios, local)
+    #B+    
+    notificacao(Bp, local, "B+")
 
-        #AB+
-        notificacao(ABp, usuarios, local)
+    #B-    
+    notificacao(Bn, local, "B+")
 
-        #AB-
-        notificacao(ABn, usuarios, local)
+    #AB+    
+    notificacao(ABp, local, "AB+")
 
-        #O+
-        notificacao(Op, usuarios, local)
+    #AB-    
+    notificacao(ABn, local, "AB-")
 
-        #O-
-        notificacao(On, usuarios, local)
+    #O+    
+    notificacao(Op, local, "O+")
+
+    #O-    
+    notificacao(On, local, "O-")
 
     flash("ATUALIZADO COM SUCESSO")
     return redirect('/')
